359752737e93a4fe9c9ae0e76312c741d79fc4fd
Ahmadwali zadron <wzadron@msudenver.edu>
2019-10-23 14:42:59 -0600

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :parts
+  resources :makes
+  resources :cars do
+    collection do
+      get 'search'
+    end
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
